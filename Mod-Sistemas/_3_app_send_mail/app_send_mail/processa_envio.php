8762f80044b069c17ae3b199692df38cd723cb8f
LISANDRO PEIXER <56092210+LisandroPeixer@users.noreply.github.com>
2020-03-24 15:18:18 -0300

    Update processa_envio.php

--- Mod-Sistemas/_3_app_send_mail/app_send_mail/processa_envio.php
+++ Mod-Sistemas/_3_app_send_mail/app_send_mail/processa_envio.php
@@ -51,8 +51,8 @@
 	    $mail->isSMTP();                                // Set mailer to use SMTP
 	    $mail->Host = 'smtp.gmail.com';  				// Specify main and backup SMTP servers
 	    $mail->SMTPAuth = true;                         // Enable SMTP authentication
-	    $mail->Username = 'pxrtecnologia@gmail.com';     // SMTP username
-	    $mail->Password = 'lisandro@peixer.com';                   // SMTP password
+	    $mail->Username = ';    			    // SMTP username
+	    $mail->Password = '';                           // SMTP password
 	    $mail->SMTPSecure = 'tls';                      // Enable TLS encryption, `ssl` also accepted
 	    $mail->Port = 587;                              // TCP port to connect to
 
